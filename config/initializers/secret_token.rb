529d5389d2ed5965ae15a1b26f9854699f8e6203
sergiosouzalima <sergiosouzalima@gmail.com>
2011-01-26 14:33:46 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Connectioncrm::Application.config.secret_token = '0aaf910bdbc3e998d9fc617a5cf0a0217459d629f8b306d991586bf98989cd8e798e47167f6ab81a33a29e66a7c75042837ab7add77e5a320ca558d97556c902'
